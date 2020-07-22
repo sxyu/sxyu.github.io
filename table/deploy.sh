1aa585f3fd8598bd5d6d407d19261dccbbe9f987
Alex Yu <sxyu@berkeley.edu>
2020-07-21 22:58:35 -0700

    Updated projects page, deleted table

--- table/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ~/proj/cc-tools/web-table
-scp index.html ocf:public_html/table
-scp css/style.css    ocf:public_html/table/css
-scp js/main.js  ocf:public_html/table/js
-scp data/*.json  ocf:public_html/table/data
